c6035f667cc072c676c5a9361b5e9ae7eba14515
Sam Edwards <sam.edwards@bc.libraries.coop>
2021-03-02 13:42:09 -0800

    Code cleanup, PSR12 standard, add composer file

--- class-libpress-lib-value-calc.php
+++ class-libpress-lib-value-calc.php
@@ -1,183 +1,222 @@
-<?php defined('ABSPATH') || die(-1);
+<?php
 
 /**
- * Plugin Name: LibPress Library Value Calculator
- * Description: Options and GravityForm for library value calculators
- * Author: Jonathan Schatz, BC Libraries Cooperative
- * Author URI: https://bc.libraries.coop
- * Version: 0.1.0
- **/
+ * LibPress Library Value Calculator
+ *
+ * Options and GravityForm for library value calculators
+ *
+ * PHP Version 7
+ *
+ * @package           BCLibCoop\LibValueCalc
+ * @author            Jonathan Schatz <jonathan.schatz@bc.libraries.coop>
+ * @author            Sam Edwards <sam.edwards@bc.libraries.coop>
+ * @copyright         2016-2021 BC Libraries Cooperative
+ * @license           GPL-2.0-or-later
+ *
+ * @wordpress-plugin
+ * Plugin Name:       LibPress Library Value Calculator
+ * Description:       Options and GravityForm for library value calculators
+ * Version:           1.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.0
+ * Author:            BC Libraries Cooperative
+ * Author URI:        https://bc.libraries.coop
+ * Text Domain:       libpress-value-calc
+ * License:           GPL v2 or later
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ */
 
-if (!class_exists('LibValueCalc')):
+namespace BCLibCoop;
 
-	class LibValueCalc {
-		
-		protected $slug = 'lib-value-';
+class LibValueCalc
+{
+    private static $instance;
 
-		protected $fields = array();
+    public $slug = 'lib-value-';
+    public $fieldset = [];
 
-		public function __construct() {
-			$this->fieldset = array(
-                (object) [ 'internal_name' => 'books', 'public_name' => 'Books Read', 'description' => 'Average print book price', 'value' => 0.00],
-				(object) [ 'internal_name' => 'magazines', 'public_name' => 'Magazines Read', 'description' => 'Average retail price of a magazine', 'value' => 0.00],
-				(object) [ 'internal_name' => 'dvds', 'public_name' => 'DVDs Watched', 'description' => 'Rental price of a new release DVD movie', 'value' => 0.00],
-				(object) [ 'internal_name' => 'games', 'public_name' => 'Video Games Played', 'description' => 'Rental price of a new video game', 'value' => 0.00],
-				(object) [ 'internal_name' => 'cds', 'public_name' => 'Music CDs Listened to', 'description' => 'Average price of a music CD from iTunes', 'value' => 0.00],
-				(object) [ 'internal_name' => 'ebooks', 'public_name' => 'eBooks/Audiobooks Downloaded', 'description' => 'Average download price of a recently published eBook or audiobook', 'value' => 0.00],
-				(object) [ 'internal_name' => 'holds', 'public_name' => 'Holds Placed', 'description' => 'Cost of ILL service per patron', 'value' => 0.00],
-				(object) [ 'internal_name' => 'questions', 'public_name' => 'Questions Answered', 'description' => 'Cost of staffing information desk/ask service per patron', 'value' => 0.00],
-				(object) [ 'internal_name' => 'computer', 'public_name' => 'Public Computer Use 60 mins/day', 'description' => 'Average price of hour of computer/internet time', 'value' => 0.00],
-				(object) [ 'internal_name' => 'programs', 'public_name' => 'Programs and Classes Attended', 'description' => 'Average cost of delivering workshops and programming per patron', 'value' => 0.00],
-                (object) [ 'internal_name' => 'property-tax', 'public_name' => 'Total you paid for these monthly services', 'description' => 'Monthly property tax collected from average home value in community', 'value' => 0.00],
-				);
-
-			add_action('init', array(&$this, '_init'));
-            add_filter( 'gform_field_value', array( &$this, 'populate_fields'), 10, 3 );
-
-		}
-
-		public function _init() {
-
-            if ( is_admin() ) {
-                add_action( 'admin_enqueue_scripts', array(&$this, 'admin_enqueue_styles_scripts') );
-                add_action( 'admin_menu', array(&$this, 'add_lib_calc_menu') );
-                add_action( 'wp_ajax_' . $this->slug . 'save-change', array(&$this, 'lib_value_calc_save_change_callback')) ;
-            } else {
-                add_action( 'wp_enqueue_scripts', array(&$this, 'frontside_enqueue_styles_scripts') );
-            }
-
-            //default options if not already set
-            add_option($this->slug . 'options', $this->fieldset, '', 'no'); //Store the entire initial object, no autoload
-
-		}
-
-        /**
-         * Match each option to form field and provide to filter for dynamic population.
-         *
-         * @param string $value The string containing the current field value to be filtered.
-         * @param object $field The current field being processed
-         * @param string $field_name The parameter name of the field or input being processed.
-         * @return mixed
-         */
-
-        public function populate_fields($value, $field, $field_name ) {
-		    $results = $this->check_obj_cache( 'lib_value_calc_opts_cache' );
-
-            $values = array();
-		    foreach( $results as $fieldObj ) {
-                $our_internal_name = 'lib_value_' . $fieldObj->internal_name;
-                if ( $field_name === $our_internal_name ) $values[$our_internal_name] = $fieldObj->value;
-            }
-
-            return isset( $values[ $field_name ] ) ? $values[ $field_name ] : $value;
+    public function __construct()
+    {
+        if (isset(self::$instance)) {
+            return;
         }
 
-        //Later use memcache?
-        public function check_obj_cache( $key ) {
+        self::$instance = $this;
 
-            $cached = wp_cache_get( $key );
-            if ( false === $cached ) {
-                $cached = get_option( $this->slug . 'options' );
-                wp_cache_set( $key, $cached, '', 60 ); //one minute, same request
+        $this->fieldset = [
+            (object) ['internal_name' => 'books', 'public_name' => 'Books Read',
+                'description' => 'Average print book price', 'value' => 0.00],
+            (object) ['internal_name' => 'magazines', 'public_name' => 'Magazines Read',
+                'description' => 'Average retail price of a magazine', 'value' => 0.00],
+            (object) ['internal_name' => 'dvds', 'public_name' => 'DVDs Watched',
+                'description' => 'Rental price of a new release DVD movie', 'value' => 0.00],
+            (object) ['internal_name' => 'games', 'public_name' => 'Video Games Played',
+                'description' => 'Rental price of a new video game', 'value' => 0.00],
+            (object) ['internal_name' => 'cds', 'public_name' => 'Music CDs Listened to',
+                'description' => 'Average price of a music CD from iTunes', 'value' => 0.00],
+            (object) ['internal_name' => 'ebooks', 'public_name' => 'eBooks/Audiobooks Downloaded',
+                'description' => 'Average download price of a recently published eBook or audiobook', 'value' => 0.00],
+            (object) ['internal_name' => 'holds', 'public_name' => 'Holds Placed',
+                'description' => 'Cost of ILL service per patron', 'value' => 0.00],
+            (object) ['internal_name' => 'questions', 'public_name' => 'Questions Answered',
+                'description' => 'Cost of staffing information desk/ask service per patron', 'value' => 0.00],
+            (object) ['internal_name' => 'computer', 'public_name' => 'Public Computer Use 60 mins/day',
+                'description' => 'Average price of hour of computer/internet time', 'value' => 0.00],
+            (object) ['internal_name' => 'programs', 'public_name' => 'Programs and Classes Attended',
+                'description' => 'Average cost of delivering workshops and programming per patron', 'value' => 0.00],
+            (object) ['internal_name' => 'property-tax', 'public_name' => 'Total you paid for these monthly services',
+                'description' => 'Monthly property tax collected from average home value in community',
+                'value' => 0.00],
+        ];
+
+        add_action('init', [&$this, 'init']);
+    }
+
+    public function init()
+    {
+        if (is_admin()) {
+            add_action('admin_enqueue_scripts', [&$this, 'adminEnqueueStylesScripts']);
+            add_action('admin_menu', [&$this, 'addLibCalcMenu']);
+            add_action('wp_ajax_' . $this->slug . 'save-change', [&$this, 'libValueCalcSaveChangeCallback']);
+        } else {
+            add_action('wp_enqueue_scripts', [&$this, 'frontsideEnqueueStylesScripts']);
+        }
+
+        // default options if not already set
+        add_option($this->slug . 'options', $this->fieldset);
+
+        add_filter('gform_field_value', [&$this, 'populateFields'], 10, 3);
+    }
+
+    public function adminEnqueueStylesScripts($hook)
+    {
+        if ('site-manager_page_' . $this->slug . 'options' !== $hook) {
+            return;
+        }
+
+        wp_enqueue_script(
+            $this->slug . 'calc-admin-js',
+            plugins_url('/js/' . $this->slug . 'calc-admin.js', __FILE__),
+            ['jquery']
+        );
+    }
+
+    public function frontsideEnqueueStylesScripts()
+    {
+        wp_enqueue_style(
+            $this->slug . 'form-front-css',
+            plugins_url('/css/' . $this->slug . 'form-front.css', __FILE__)
+        );
+    }
+
+    public function addLibCalcMenu()
+    {
+        add_submenu_page(
+            'site-manager',
+            'Library Value Calculator',
+            'Value Calculator Rates',
+            'manage_local_site',
+            $this->slug . 'options',
+            [&$this, 'adminLibCalcSettingsPage']
+        );
+    }
+
+    public function libValueCalcSaveChangeCallback()
+    {
+        $optionsArray = [];
+
+        foreach ($this->fieldset as $fieldObj) {
+            if (isset($_POST[$this->slug . $fieldObj->internal_name])) {
+                // updated values from form
+                $fieldObj->value = (float) sanitize_text_field($_POST[$this->slug . $fieldObj->internal_name]);
             }
-            return $cached;
-
+            $optionsArray[] = $fieldObj; // the new object array for option storage
         }
 
-		public function admin_enqueue_styles_scripts( $hook ) {
+        update_option($this->slug . 'options', $optionsArray);
 
-			if ('site-manager_page_' . $this->slug . 'options' !== $hook) {
-				return;
-			}
+        wp_send_json([
+            'result' => 'success',
+            'feedback' => 'Saved',
+        ]);
+    }
 
-			wp_register_script($this->slug . 'calc-admin-js', plugins_url('/js/' . $this->slug . 'calc-admin.js', __FILE__), array('jquery'));
-			wp_enqueue_script($this->slug . 'calc-admin-js');
-
-		}
-
-		public function add_lib_calc_menu() {
-			add_submenu_page('site-manager', 'Library Value Calculator', 'Value Calculator Rates', 'manage_local_site', $this->slug . 'options', array(&$this, 'admin_lib_calc_settings_page'));
-		}
-
-		public function frontside_enqueue_styles_scripts( $hook ) {
-		    wp_register_style( $this->slug . 'form-front-css', plugins_url( '/css/' . $this->slug . 'form-front.css', __FILE__ ), false );
-            wp_enqueue_style( $this->slug . 'form-front-css' );
+    /**
+     * Store value field as options per Co-op client library
+     **/
+    public function adminLibCalcSettingsPage()
+    {
+        if (!current_user_can('manage_local_site')) {
+            wp_die('You do not have required permissions to view this page');
         }
 
-		public function lib_value_calc_save_change_callback() {
+        $out = [];
+        $out[] = '<div class="wrap">';
 
-			$optionsArray = array();
+        $out[] = '<div id="icon-options-general" class="icon32">';
+        $out[] = '<br>';
+        $out[] = '</div>';
 
-			foreach ( $this->fieldset as $fieldObj ) {
-				if ( isset($_POST[$this->slug . $fieldObj->internal_name]) ) {
-					$fieldObj->value = (float) sanitize_text_field($_POST[$this->slug . $fieldObj->internal_name]); //updated values from form
-				}
-				$optionsArray[] = $fieldObj; //the new object array for option storage
-			}
+        $out[] = '<h2>Library Value Rates</h2>';
 
-			update_option($this->slug . 'options', $optionsArray);
+        $out[] = '<table class="form-table">';
 
-			echo '{"result":"success","feedback":"Saved"}';
-			die();
-		}
+        $stored = get_option($this->slug . 'options');
 
-		/**
-		 *	Store value field as options
-		 *	per Co-op client library
-		 *
-		 **/
-		public function admin_lib_calc_settings_page() {
+        foreach ($stored as $obj) {
+            $name = (string) $obj->internal_name;
+            $public = (string) $obj->public_name;
+            $value = (isset($obj->value)) ? $obj->value : '0.00';
+            $description = $obj->description;
 
-			if (!current_user_can('manage_local_site')) {
-				die('You do not have required permissions to view this page');
-			}
+            $out[] = '<tr valign="top">';
+            $out[] = '<th scope="row">';
+            $out[] = '<label for="' . $this->slug . $name . '">' . ucfirst($name) . '</label>';
+            // $out[] = '<p class="' . $this->slug . $name . '-public' . '">' . ucfirst($public) . '</p>';
+            $out[] = '</th>';
+            $out[] = '<td>';
+            $out[] = '$ <input type="text" id="' . $this->slug . $name . '" name="' . $this->slug . $name
+                     . '"  value="' . $value . '">';
+            $out[] = '<legend><em>' . $description . '</em></legend>';
+            $out[] = '</td>';
+            $out[] = '</tr>';
+        }
 
-			$out = array();
-			$out[] = '<div class="wrap">';
+        $out[] = '</table>';
 
-			$out[] = '<div id="icon-options-general" class="icon32">';
-			$out[] = '<br>';
-			$out[] = '</div>';
+        $out[] = '<p class="submit">';
+        $out[] = '<input type="submit" value="Save Changes" class="button button-primary" id="' . $this->slug
+                 . 'submit" name="submit">';
+        $out[] = '</p>';
 
-			$out[] = '<h2>Library Value Rates</h2>';
+        echo implode("\n", $out);
+    }
 
-			$out[] = '<table class="form-table">';
+    /**
+     * Match each option to form field and provide to filter for dynamic population.
+     *
+     * @param string $value The string containing the current field value to be filtered.
+     * @param object $field The current field being processed
+     * @param string $field_name The parameter name of the field or input being processed.
+     *
+     * @return mixed
+     */
+    public function populateFields($value, $field, $field_name)
+    {
+        if (strpos($field_name, 'lib_value_') === 0) {
+            $fieldset = get_option($this->slug . 'options');
 
-			$stored = get_option( $this->slug . 'options' );
+            foreach ($fieldset as $fieldObj) {
+                if ($field_name === 'lib_value_' . $fieldObj->internal_name) {
+                    return $fieldObj->value;
+                }
+            }
+        }
 
-			foreach ($stored as $obj) {
+        return $value;
+    }
+}
 
-                $name = (string) $obj->internal_name;
-                $public = (string) $obj->public_name;
-                $value = (isset($obj->value)) ? $obj->value: '0.00';
-                $description = $obj->description;
+defined('ABSPATH') || die(-1);
 
-				$out[] = '<tr valign="top">';
-				$out[] = '<th scope="row">';
-                $out[] = '<label for="' . $this->slug . $name . '">' . ucfirst( $name ) . '</label>';
-                //$out[] = '<p class="' . $this->slug . $name . '-public' . '">' . ucfirst( $public ) . '</p>';
-				$out[] = '</th>';
-				$out[] = '<td>';
-				$out[] = '$ <input type="text" id="' . $this->slug . $name . '" name="' . $this->slug . $name . '"  value="' . $value . '">';
-                $out[] = '<legend><em>'.$description.'</em></legend>';
-				$out[] = '</td>';
-				$out[] = '</tr>';
-			}
-
-			$out[] = '</table>';
-
-			$out[] = '<p class="submit">';
-			$out[] = '<input type="submit" value="Save Changes" class="button button-primary" id="' . $this->slug . 'submit" name="submit">';
-			$out[] = '</p>';
-
-			echo implode("\n", $out);
-		}
-
-
-	} //class
-
-	if (!isset($libcalc)) {
-		global $libcalc;
-		$libcalc = new LibValueCalc();
-	}
-endif; /* no singleton */
+new LibValueCalc();
--- js/lib-value-calc-admin.js
+++ js/lib-value-calc-admin.js
@@ -5,53 +5,51 @@
  * Author URI: https://bc.libraries.coop
  * Version: 0.1.0
  **/
- 
- ;(function($,window){
-	 
-	 var self;
-	 
-	 var LibValueCalcAdmin = function(opts){
-		self = this;
-		self.init(opts);
-	 }
-	 
-	 LibValueCalcAdmin.prototype = {
-		 
-		 init: function(opts) {
-		 	$('#lib-value-submit').click( self.submit_form );
-		 	return this;			 
-		 },
-		 
-		 submit_form: function() {
-			 
-			 var data = {
-				 action: 'lib-value-save-change',
-				 "lib-value-books":  $('#lib-value-books').val(),
-				 "lib-value-magazines":  $('#lib-value-magazines').val(),
-				 "lib-value-dvds":  $('#lib-value-dvds').val(),
-				 "lib-value-games":  $('#lib-value-games').val(),
-				 "lib-value-cds":  $('#lib-value-cds').val(),
-				 "lib-value-ebooks":  $('#lib-value-ebooks').val(),
-				 "lib-value-holds":  $('#lib-value-holds').val(),
-				 "lib-value-computer":  $('#lib-value-computer').val(),
-				 "lib-value-questions":  $('#lib-value-questions').val(),
-				 "lib-value-programs":  $('#lib-value-programs').val(),
-				 "lib-value-property-tax":  $('#lib-value-property-tax').val(),
-			 }
-			 
-			 $.post( ajaxurl, data ).complete(function(r){
-			 	var res = JSON.parse(r.responseText);
-			 		alert( res.feedback );
-			 });
-		 }
-	 }
-	 
-	 $.fn.libvaluecalc = function(opts) {
-		 return new LibValueCalcAdmin(opts);
-	 }
-	 
- }(jQuery,window))
- 
- jQuery().ready(function($){
-	 window.libvaluecalc = $().libvaluecalc(); 
- });
+
+; (function ($, window) {
+
+  var self;
+
+  var LibValueCalcAdmin = function (opts) {
+    self = this;
+    self.init(opts);
+  }
+
+  LibValueCalcAdmin.prototype = {
+
+    init: function (opts) {
+      $('#lib-value-submit').click(self.submit_form);
+      return this;
+    },
+
+    submit_form: function () {
+
+      var data = {
+        action: 'lib-value-save-change',
+        "lib-value-books": $('#lib-value-books').val(),
+        "lib-value-magazines": $('#lib-value-magazines').val(),
+        "lib-value-dvds": $('#lib-value-dvds').val(),
+        "lib-value-games": $('#lib-value-games').val(),
+        "lib-value-cds": $('#lib-value-cds').val(),
+        "lib-value-ebooks": $('#lib-value-ebooks').val(),
+        "lib-value-holds": $('#lib-value-holds').val(),
+        "lib-value-computer": $('#lib-value-computer').val(),
+        "lib-value-questions": $('#lib-value-questions').val(),
+        "lib-value-programs": $('#lib-value-programs').val(),
+        "lib-value-property-tax": $('#lib-value-property-tax').val(),
+      }
+
+      $.post(ajaxurl, data).complete(function (res) {
+        alert(res.feedback);
+      });
+    }
+  }
+
+  $.fn.libvaluecalc = function (opts) {
+    return new LibValueCalcAdmin(opts);
+  }
+}(jQuery, window))
+
+jQuery().ready(function ($) {
+  window.libvaluecalc = $().libvaluecalc();
+});
